09cd91f6e12bf53dd3879a7ca117401d4e6c99ca
Leemainsw <s2018s15@e-mirim.hs.kr>
2020-05-22 09:42:52 +0900

    초기 커밋.

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void T01_더하기()
+        {
+            int a = 5;
+            int b = 5;
+
+            int expected = 10;
+
+            Assert.AreEqual(expected, a + b, "Add Failed");
+        }
+        
+
+        [TestMethod]
+        public void T02_빼기()
+        {
+            int a = 10;
+            int b = 5;
+            int expected = 5;
+
+            Assert.AreEqual(expected, a - b, "Minus Failed");
+        }
+
+        [TestMethod]
+        public void T03_1부터_10까지_더하기()
+        {
+            int sum = 0;
+            for(int i=0; i<10; i++)
+            {
+                sum += i + 1;
+            }
+
+            int expected = 55;
+
+            Assert.AreEqual(expected, sum, "Failed");
+        }
+    }
+}
